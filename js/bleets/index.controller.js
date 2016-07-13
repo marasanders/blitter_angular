f14a6d752ecb16134f9e989375665a14d501a99b
Mara Sanders <sandersmara@gmail.com>
2016-07-13 14:21:41 -0400

    added delete function on index page

--- js/bleets/index.controller.js
+++ js/bleets/index.controller.js
@@ -5,12 +5,18 @@
   .module("bleets")
   .controller("BleetIndexController", [
     "BleetFactory",
-    BleetIndexControllerFunction
+     BleetIndexControllerFunction
   ]);
 
 
   function BleetIndexControllerFunction(BleetFactory){
-    this.bleets = BleetFactory.query();
+     this.bleets = BleetFactory.query();
+     var self = this
+     this.destroy = function(id){
+     this.bleet = BleetFactory.get({id: id});
+     this.bleet.$delete({id: id}).then(function() {
+     self.bleets = BleetFactory.query()
+     })
+   }
   }
-
 }());
